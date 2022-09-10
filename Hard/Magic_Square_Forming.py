10fba683ef88fee445c4e33f2234b1eca557c529
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-10 01:32:34 -0700

    trial_attempt

--- Hard/Magic_Square_Forming.py
+++ Hard/Magic_Square_Forming.py
@@ -4,12 +4,36 @@ import random
 import re
 import sys
 
-def formingMagicSquare(s):
-    sum=0
+def flipMatrix(m1):
+    m2 =[[0 for i in range(len(m1[0]))] for i in range(len(m1[0]))]
+    m2[int(len(m1[0])/2)][int(len(m1[0])/2)]==m1[int(len(m1[0])/2)][int(len(m1[0])/2)]
+    a,b=0,1
+    for i in range(len(m1[0])):
+        for j in range(len(m1[0])):
+            if m2[a][b] != 0:
+                continue
+            else:
+                m2[a][b]= m1[i][j]
+                if b<= len(m1[0]):
+                    b+=1
+                else:
+                    if a<3:
+                         a+=1
+                    else:
+                        a=0
+                    b=0
+    return m2
+
+
+
+def formingMagicSquare(s,iter,sum):
+    if iter==7:
+        return sum
+    count=0
     a,b=len(s[0])/2,(len(s[0])-1)
     counter=[[a,b]]
     for i in range(1,10):
-        sum+= abs(s[a][b]-i)
+        count+= abs(s[a][b]-i)
         a+=-1
         b+=1
         if [a,b] not in counter:
@@ -17,6 +41,10 @@ def formingMagicSquare(s):
         else:
             a-=2
             b+=1
+    sum=max(sum,count)
+    iter+=1
+    formingMagicSquare(flipMatrix(s),iter,sum)
+    
     
 
 if __name__ == '__main__':
@@ -27,7 +55,7 @@ if __name__ == '__main__':
     for _ in range(3):
         s.append(list(map(int, input().rstrip().split())))
 
-    result = formingMagicSquare(s)
+    result = formingMagicSquare(s,0,0)
 
     fptr.write(str(result) + '\n')
 
