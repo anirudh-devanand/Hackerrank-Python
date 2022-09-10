1d75a9eefcdaab34fc5610ab5e85486a04b24b99
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-10 15:11:23 -0700

    fixed_error: returns value

--- Hard/Magic_Square_Forming.py
+++ Hard/Magic_Square_Forming.py
@@ -4,59 +4,64 @@ import random
 import re
 import sys
 
-def flipMatrix(m1):
-    m2 =[[0 for i in range(len(m1[0]))] for i in range(len(m1[0]))]
-    m2[int(len(m1[0])/2)][int(len(m1[0])/2)]==m1[int(len(m1[0])/2)][int(len(m1[0])/2)]
-    a,b=0,1
-    for i in range(len(m1[0])):
-        for j in range(len(m1[0])):
-            if m2[a][b] != 0:
-                continue
-            else:
-                m2[a][b]= m1[i][j]
-                if b<= len(m1[0]):
-                    b+=1
-                else:
-                    if a<3:
-                         a+=1
-                    else:
-                        a=0
-                    b=0
-    return m2
-
-
+def flipMatrix(m1,iter):
+    if iter<=4:
+        j=0
+    else:
+        j=1
+    temp = m1[0][j]
+    m1[0][j] = m1[2- j][0]
+    m1[2 - j][0] = m1[2][2 - j]
+    m1[2][2 - j] = m1[j][2]
+    m1[j][2] = temp
+    return m1
 
 def formingMagicSquare(s,iter,sum):
     if iter==7:
-        return sum
+        print (sum)
+        return
     count=0
-    a,b=len(s[0])/2,(len(s[0])-1)
+    a=int(len(s[0])/2)
+    b=int(len(s[0])-1)
     counter=[[a,b]]
     for i in range(1,10):
         count+= abs(s[a][b]-i)
         a+=-1
         b+=1
+        if a>2:
+            a=0
+        elif a<0:
+            a=2
+        if b>2:
+            b=0
         if [a,b] not in counter:
             counter.append([a,b])
         else:
             a-=2
             b+=1
-    sum=max(sum,count)
+            if a>2:
+                a=0
+            elif a<0:
+                a=2
+            if b>2:
+                b=0
+    sum=min(sum,count)
     iter+=1
-    formingMagicSquare(flipMatrix(s),iter,sum)
+    formingMagicSquare(flipMatrix(s,iter),iter,sum)
     
     
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#if __name__ == '__main__':
+#    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    s = []
+s = [[5,3,4],[1,5,8],[6,4,2]]
 
-    for _ in range(3):
-        s.append(list(map(int, input().rstrip().split())))
+#    for _ in range(3):
+#        s.append(list(map(int, input().rstrip().split())))
 
-    result = formingMagicSquare(s,0,0)
+formingMagicSquare(s,0,math.inf)
 
-    fptr.write(str(result) + '\n')
 
-    fptr.close()
+#    fptr.write(str(result) + '\n')
+
+#    fptr.close()
