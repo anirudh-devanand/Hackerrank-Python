424d4fb062563a7075cd892441c3f7e870e7ac01
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-10 19:57:50 -0700

    fixed_bug

--- Intermediate/FInd_Angle.py
+++ Intermediate/FInd_Angle.py
@@ -4,7 +4,7 @@ b=int(input())
 c=(math.sqrt(a**2 + b**2))
 d=math.sqrt(c**2-b**2)
 tanx=d/b
-x=((str(math.degrees(math.atan(tanx)))).split('.'))[0]
+x=round((math.degrees(math.atan(tanx))))
 degree_sign = u'\N{DEGREE SIGN}'
-print(x+degree_sign)
+print(x,degree_sign,sep='')
 
