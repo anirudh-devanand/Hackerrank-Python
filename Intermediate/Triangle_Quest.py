d4064762770dcd59b485e44f1e7ed124cccc03d5
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-11 05:46:02 -0700

    second_attempt

--- Intermediate/Triangle_Quest.py
+++ Intermediate/Triangle_Quest.py
@@ -1,5 +1,12 @@
-s,r="",""
+# s,r="",""
+# for i in range(1,int(input())+1):
+#     s+=str(i)
+#     r=str(i-1)+r
+#     print(s+r[:i-1])
+
+r,n,s=0,1,0
 for i in range(1,int(input())+1):
-    s+=str(i)
-    r=str(i-1)+r
-    print(s+r[:i-1])
+    r=((i-1)*n)+r
+    s=(s*10)+i
+    print(int(((s*n*10)+(r))/10)) 
+    n*=10
