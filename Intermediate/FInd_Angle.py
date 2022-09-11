bc78134fe5d29bb5b5cf99d90c3d4565d92a3913
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-10 19:50:36 -0700

    fixed_degree_sign

--- Intermediate/FInd_Angle.py
+++ Intermediate/FInd_Angle.py
@@ -5,5 +5,6 @@ c=(math.sqrt(a**2 + b**2))
 d=math.sqrt(c**2-b**2)
 tanx=d/b
 x=((str(math.degrees(math.atan(tanx)))).split('.'))[0]
-print(x+'°')
+degree_sign = u'\N{DEGREE SIGN}'
+print(x+degree_sign)
 
