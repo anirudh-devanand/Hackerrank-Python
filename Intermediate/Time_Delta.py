cf88cd5ce3103a6b689e894ba162cd4918c58fde
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-11 17:16:46 -0700

    second_attempt

--- Intermediate/Time_Delta.py
+++ Intermediate/Time_Delta.py
@@ -3,9 +3,10 @@ import os
 import random
 import re
 import sys
+from datetime import datetime
 
 def time_delta(t1, t2):
-    
+    delta=datetime.strptime(t1[4:15], "%d %B %Y")-datetime.strptime(t2[4:15], "%d %B %Y")
     sign1=t1.split(' ')[-1][0]
     sign2=t2.split(' ')[-1][0]
     t1diff=int(t1.split(' ')[-1])
@@ -14,26 +15,20 @@ def time_delta(t1, t2):
     if sign1 != sign2:
         absdiff-=2400
     absdiff=(int(absdiff%100)*60)+int((absdiff//100)*3600)
-    print(absdiff)
     t1=t1.split(' ')[-2]
     t2=t2.split(' ')[-2]
     abst1=(int(t1.split(':')[0])*3600)+(int(t1.split(':')[1])*60)+(int(t1.split(':')[2]))
     abst2=(int(t2.split(':')[0])*3600)+(int(t2.split(':')[1])*60)+(int(t2.split(':')[2]))
-    return(str(abs((abst1-abst2)+abs(absdiff))))
+    return(str(abs((abst1-abst2)+abs(absdiff)+abs(delta.seconds))))
 
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+t = int(input())
 
-    t = int(input())
+for t_itr in range(t):
+    t1 = input()
 
-    for t_itr in range(t):
-        t1 = input()
+    t2 = input()
 
-        t2 = input()
+    delta = time_delta(t1, t2)
 
-        delta = time_delta(t1, t2)
-
-        fptr.write(delta + '\n')
-
-    fptr.close()
+    print(delta + '\n')
