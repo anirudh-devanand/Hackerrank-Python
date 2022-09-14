e79b35f6daf5eb6ed8b86553a17b098f003337d9
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-14 00:12:44 -0700

    completed_program

--- Intermediate/Grading_Students.py
+++ Intermediate/Grading_Students.py
@@ -1,1 +1,28 @@
-round(i-(round(i/5)*5))
+import math
+import os
+import random
+import re
+import sys
+
+def gradingStudents(grades):
+    s=''
+    for i in grades:
+        if i<38:
+            s+=str(i)+'\n'
+        elif (i-(round(i//5)*5)) > 2:
+            i=(round(i//5)*5)+5
+            s+=str(i)+'\n'
+        else:
+            s+=str(i)+'\n'
+    return(s)
+                     
+
+grades_count = int(input().strip())
+
+grades = []
+
+for _ in range(grades_count):
+    grades_item = int(input().strip())
+    grades.append(grades_item)
+print(gradingStudents(grades))
+
