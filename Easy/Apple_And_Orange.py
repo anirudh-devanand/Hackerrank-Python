952bb1ab84022acac24a1d9eca660a7f8e56cf44
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-15 23:58:30 -0700

    completed_file

--- Easy/Apple_And_Orange.py
+++ Easy/Apple_And_Orange.py
@@ -5,7 +5,47 @@ import re
 import sys
 
 def countApplesAndOranges(s, t, a, b, apples, oranges):
-    
+    import math
+import os
+import random
+import re
+import sys
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    ap=0
+    for i in apples:
+        if a+i >= s and a+1 <=t:
+            ap+=1
+    ora=0
+    for i in oranges:
+        if b+i >= s and b+1 <=t:
+            ora+=1
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
+
 
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
