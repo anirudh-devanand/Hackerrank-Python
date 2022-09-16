7ebea0c3930705477383b7c23e3d9f5a9fb5ffdb
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-15 23:52:17 -0700

    edited_file

--- Easy/Breaking_The_Records.py
+++ Easy/Breaking_The_Records.py
@@ -1,17 +1,8 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'breakingRecords' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY scores as parameter.
-#
 
 def breakingRecords(scores):
     mini=scores[0]
