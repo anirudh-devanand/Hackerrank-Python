3c73a96083948c44f08427c303a8dbd680245a2c
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-17 19:39:21 -0700

    tried_using_difference_in_position

--- Intermediate/Number_Line_Jump.py
+++ Intermediate/Number_Line_Jump.py
@@ -24,12 +24,12 @@ def kangaroo(x1, v1, x2, v2):
         return('NO')
     
     if x1>x2:
-        if (x1-x2)%v2==0:
+        if (x1-x2)%v1==0:
             return('YES')
         else:
             return('NO')
     elif x2>x1:
-        if (x1-x2)%v1==0:
+        if (x1-x2)%v2==0:
             return('YES')
         else:
             return('NO')
