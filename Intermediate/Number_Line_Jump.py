8a7dc82e41a6fcefcfa75202019b8c8704e12348
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-17 19:40:10 -0700

    using_number_line_&_loop

--- Intermediate/Number_Line_Jump.py
+++ Intermediate/Number_Line_Jump.py
@@ -1,21 +1,8 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'kangaroo' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. INTEGER x1
-#  2. INTEGER v1
-#  3. INTEGER x2
-#  4. INTEGER v2
-#
 
 def kangaroo(x1, v1, x2, v2):
     if x1>x2 and v1>v2:
@@ -24,12 +11,12 @@ def kangaroo(x1, v1, x2, v2):
         return('NO')
     
     if x1>x2:
-        if (x1-x2)%v1==0:
+        if (x1-x2)%v2==0:
             return('YES')
         else:
             return('NO')
     elif x2>x1:
-        if (x1-x2)%v2==0:
+        if (x1-x2)%v1==0:
             return('YES')
         else:
             return('NO')
