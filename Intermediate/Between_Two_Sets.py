0748ac9cf0b2fd38c08457af7c4d6bf0ce7f55fc
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-17 20:20:21 -0700

    fixed_output

--- Intermediate/Between_Two_Sets.py
+++ Intermediate/Between_Two_Sets.py
@@ -34,21 +34,16 @@ def getTotalX(a, b):
     return(ans)
     
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+first_multiple_input = input().rstrip().split()
 
-    first_multiple_input = input().rstrip().split()
+n = int(first_multiple_input[0])
 
-    n = int(first_multiple_input[0])
+m = int(first_multiple_input[1])
 
-    m = int(first_multiple_input[1])
+arr = list(map(int, input().rstrip().split()))
 
-    arr = list(map(int, input().rstrip().split()))
+brr = list(map(int, input().rstrip().split()))
 
-    brr = list(map(int, input().rstrip().split()))
+total = getTotalX(arr, brr)
 
-    total = getTotalX(arr, brr)
-
-    fptr.write(str(total) + '\n')
-
-    fptr.close()
+print(total)
