46e7f37b10804532d8ed8e8d942c570cfe00911d
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-19 23:58:24 -0700

    first_attempt

---
+++ Easy/Divisble_Sum_Pairs.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+def divisibleSumPairs(n, k, ar):
+    ans=0
+    ar=ar[::-1]
+    print(ar)
+    for i in ar:
+        for j in range(i+1, len(ar)):
+            if i+ar[j]==k:
+                ans+=1
+                
+    return(ans)
+                
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
