3b82b59de54524c84b0c871215237b6aceb0104e
anirudh <62985710+anirudhd575@users.noreply.github.com>
2022-09-20 00:59:07 -0700

    chnaged_output_format

--- Intermediate/Breaking_The_Records.py
+++ Intermediate/Breaking_The_Records.py
@@ -18,16 +18,10 @@ def breakingRecords(scores):
             mi+=1
     return([ma,mi])
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+n = int(input().strip())
 
-    n = int(input().strip())
+scores = list(map(int, input().rstrip().split()))
 
-    scores = list(map(int, input().rstrip().split()))
+result = breakingRecords(scores)
 
-    result = breakingRecords(scores)
-
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
+print(result)
